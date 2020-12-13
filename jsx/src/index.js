9d988900a2621709299589217ef0fc929b40a102
cjwbrandon <cjwbrandon@gmail.com>
2020-12-13 15:21:28 +0800

    [ADD] Difference between JSX and HTML

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,9 +3,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create a react component
+// function getButtonText() {
+//   return "Click on me!";
+// }
+
 // Function-based component
 const App = function () {
-  return <div>Hi there!</div>;
+  // Valid values
+  // const buttonText = "Click me";
+  // const buttonText = 12345;
+  // const buttonText = ["Hi", "There"];
+  // const buttonText = [10, 20];
+
+  // Invalid values
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
