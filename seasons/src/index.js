e0e364700f48284dfc8408ac4c2238e8c4cb3e54
cjwbrandon <cjwbrandon@gmail.com>
2020-12-14 21:54:17 +0800

    [ADD] Lifecycle Methods and Class Components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 // Functional Component
 // const App = () => {
@@ -14,27 +16,56 @@ import ReactDOM from "react-dom";
 // Class Based Component
 class App extends React.Component {
   // Specific to JS and not React
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
-    // Initialise state -> must be this.state
-    this.state = { lat: null, errorMessage: "" };
+  //   // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
+  //   // Initialise state -> must be this.state
+  //   this.state = { lat: null, errorMessage: "" };
 
+  //   // We can do data loading in constructor but best practice to move it to componentDidMount
+  //   // window.navigator.geolocation.getCurrentPosition(
+  //   //   (position) => {
+  //   //     // We called setState -> part of React.Component
+  //   //     // .setState({{changesObject}})
+  //   //     this.setState({ lat: position.coords.latitude });
+
+  //   //     // Do not directly manipulate
+  //   //     // this.state.lat = position.coords.latitude;
+  //   //   },
+  //   //   // Error Handling
+  //   //   (err) => {
+  //   //     this.setState({ errorMessage: err.message });
+  //   //   }
+  //   // );
+  // }
+
+  // Alternative State Initialisation -> do not need constructor anymore
+  state = { lat: null, errorMessage: "" };
+
+  // Do data loading in componentDidMount
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // We called setState -> part of React.Component
-        // .setState({{changesObject}})
-        this.setState({ lat: position.coords.latitude });
-
-        // Do not directly manipulate
-        // this.state.lat = position.coords.latitude;
-      },
-      // Error Handling
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
+  }
+
+  componentDidUpdate() {
+    console.log("My component was just updated it rerendered!");
+  }
+
+  // Place conditional logic in a helper function
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      // SeasonDisplay gets rerendered as well -> parent state change -> child rerenders
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
   }
 
   // React says we have to define render!! Required
@@ -44,16 +75,18 @@ class App extends React.Component {
     //   (position) => (this.lat = position.coords.latitude),
     //   (err) => console.log(err)
     // );
-
     // Conditional Rendering -> Simplest way
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }
+    // Avoid Conditionals in Render -> difficult to make changes in the future.
+    // if (this.state.errorMessage && !this.state.lat) {
+    //   return <div>Error: {this.state.errorMessage}</div>;
+    // }
+    // if (!this.state.errorMessage && this.state.lat) {
+    //   // SeasonDisplay gets rerendered as well -> parent state change -> child rerenders
+    //   return <SeasonDisplay lat={this.state.lat} />;
+    // }
+    // return <Spinner message="Please accept location request" />;
 
-    return <div>Loading!</div>;
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
