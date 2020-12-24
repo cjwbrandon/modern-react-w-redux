4b3c46c7d7db66ce5ac868d09b629ef1c0f972e5
cjwbrandon <cjwbrandon@gmail.com>
2020-12-24 10:20:53 +0800

    [ADD] All REST calls in action creators

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -35,13 +37,13 @@ class StreamCreate extends React.Component {
   };
 
   // Field values are pasrsed as input
-  onSubmit(formValues) {
+  onSubmit = (formValues) => {
     // Redux-form takes care of this for us
     // event.preventDefault();
-    console.log(formValues);
+    // console.log(formValues);
 
-    // Input validation
-  }
+    this.props.createStream(formValues);
+  };
 
   render() {
     // add className of 'error' to show error messages
@@ -63,6 +65,7 @@ class StreamCreate extends React.Component {
   }
 }
 
+// Input Validation
 const validate = (formValues) => {
   const errors = {};
   // Errors properties must be the same as the name of the Field!
@@ -73,4 +76,6 @@ const validate = (formValues) => {
 };
 
 // reduxForm -> Similar to connect
-export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
+const formWrapped = reduxForm({ form: "streamCreate", validate })(StreamCreate);
+// Joining connect with reduxForm
+export default connect(null, { createStream })(formWrapped);
