307275f731a7fb62f4f122453731303f6606f3b9
cjwbrandon <cjwbrandon@gmail.com>
2020-12-29 13:45:15 +0800

    [ADD] Routes and Components outline

--- streams/client_func/src/components/App.js
+++ streams/client_func/src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
 
-const App = (props) => {
-  return <div>App</div>;
+import StreamOverview from "./streams/StreamOverview";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Route path="/" exact component={StreamOverview} />
+        <Route path="/streams/create" exact component={StreamCreate} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ streams/client_func/src/components/streams/StreamOverview.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamOverview = (props) => {
+  return <div>Stream Overview</div>;
+};
+
+export default StreamOverview;
